20f26aa3aadceb0479226d1df3407303d34368c9
UmangKhandelwal16 <umangkhandelwal1604@gmail.com>
2023-04-18 23:37:56 +0530

    updated training section

--- src/Components/ImageSlider.jsx
+++ src/Components/ImageSlider.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
-import Products from './Products'
 import './ImageSlider.css'
 
+let Products = [
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/1954524/pexels-photo-1954524.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    },
+    
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+
+    {
+        url : "https://images.pexels.com/photos/3768916/pexels-photo-3768916.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        caption : 'Cardio workout'
+    }, 
+    ]
+
+
 function ImageSlider() {
-    let box = document.querySelector('.Products-container')
+    let box = document.querySelector('.Product-container')
     const prvsbtn = () => {
         let width = box.clientWidth
         box.scrollLeft = box.scrollLeft - width
@@ -15,23 +57,29 @@ function ImageSlider() {
     }
 
   return (
+    <>
     <div className='Scroller'>
         <button className='pre-btn' onClick = {prvsbtn}> <p> &lt; </p></button>
         <button className = 'next-btn' onClick = {nextbtn}> <p> &gt; </p></button>
-
-
-
-        <div className='Products-container'>
-            <Products Productno = '1'/>
-            <Products Productno = '2'/>
-            <Products Productno = '3'/>
-            <Products Productno = '4'/>
-            <Products Productno = '5'/>
-            <Products Productno = '6'/>
-            <Products Productno = '7'/>
+      
+        <div className = "Product-container">
+            {
+                Products.map((data) => (
+                    <div key = {data.id} className = "product-cards">
+                        <div className = "product-images">
+                            <img src = {data.url} alt = {data.caption} className = "products"/>
+                            
+                        </div>
+                        <div>
+                        <h3 className='caption'>{data.caption}</h3>
+                        </div>
+                    </div>
+                )
+                )
+            }
         </div>
-
-    </div>
+    </div> 
+    </>
   )
 }
 
