55d961165e334a9da372774db619e8d04c3fe341
mondalnilavra13 <mondalnilavra13@gmail.com>
2023-04-19 01:34:37 +0530

    Adding pricing section

---
+++ src/components/Pricing/Pricing.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import style from './Pricing.module.css'
+
+function GymPricing() {
+    return (
+    <div className={style.mainContainer}>
+      <div className={style.pricingHeading}>
+        <h1 className={style.pricingHeading}>Our Plans</h1>
+    </div>
+    <div className={style.pricingContainer}>
+    
+    <div className={style.pricingPlan}>
+      <h2>Basic</h2>
+      <h3>$19.99/month</h3>
+      <ul>
+        <li>Access to gym during regular hours</li>
+        <li>Basic workout equipment</li>
+        <li>No personal training sessions</li>
+      </ul>
+      <button>Sign Up</button>
+    </div>
+    <div className={style.pricingPlan}>
+      <h2>Premium</h2>     
+      <h3>$49.99/month</h3>
+      <ul>
+        <li>24/7 gym access</li>
+        <li>Full range of workout equipment</li>
+        <li>1 personal training session/month</li>
+      </ul>
+      <button>Sign Up</button>
+    </div>
+    <div className={style.pricingPlan}>
+      <h2>Elite</h2>
+      <h3>$99.99/month</h3>
+      <ul>
+        <li>24/7 gym access</li>
+        <li>Full range of workout equipment</li>
+        <li>2 personal training sessions/month</li>
+      </ul>
+      <button>Sign Up</button>
+    </div>
+  </div>
+
+    </div>
+
+    
+    
+    );
+}
+
+export default GymPricing;
