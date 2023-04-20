2138e21f6bd163009399165f0c6e87fba24bae2b
nlprasanna5 <lakshmi.76740@gmail.com>
2023-04-20 15:33:40 +0530

    included responsive behaviour for pricing and training section

--- src/components/Pricing/Pricing.jsx
+++ src/components/Pricing/Pricing.jsx
@@ -3,55 +3,56 @@ import style from './Pricing.module.css';
 import { useNavigate } from 'react-router-dom';
 
 function GymPricing() {
-  const navigate=useNavigate();
-    return (
+  const navigate = useNavigate();
+  return (
     <div id='pricing' className={style.mainContainer}>
-      <div className={style.pricingHeading}>
-        <h1 className={style.pricingHeading}>Our Plans
+      <div className={style.price}>
+        <div className={style.pricingHeading}>
+          <h1 className={style.plan}>Our Plans
             <h6>"Get Fit and Save Big with </h6>
             <h6>Our Gym Membership Plans -  </h6>
-         <h6>Choose the Best Option for You!"</h6>
-        </h1>
-    </div>
-    <div className={style.pricingContainer}>
-    
-    <div className={style.pricingPlan}>
-      <h2>Basic</h2>
-      <h3>$19.99/month</h3>
-      <ul>
-        <li>Access to gym during regular hours</li>
-        <li>Basic workout equipment</li>
-        <li>No personal training sessions</li>
-      </ul>
-      <button onClick={()=>navigate('/register')}>Register</button>
-    </div>
-    <div className={style.pricingPlan}>
-      <h2>Premium</h2>     
-      <h3>$49.99/month</h3>
-      <ul>
-        <li>24/7 gym access</li>
-        <li>Full range of workout equipment</li>
-        <li>1 personal training session/month</li>
-      </ul>
-      <button onClick={()=>navigate('/register')}>Register</button>
-    </div>
-    <div className={style.pricingPlan}>
-      <h2>Elite</h2>
-      <h3>$99.99/month</h3>
-      <ul>
-        <li>24/7 gym access</li>
-        <li>Full range of workout equipment</li>
-        <li>2 personal training sessions/month</li>
-      </ul>
-      <button onClick={()=>navigate('/register')}>Register</button>
-    </div>
-  </div>
+            <h6>Choose the Best Option for You!"</h6>
+          </h1>
+        </div>
+        <div className={style.pricingContainer}>
 
+          <div className={style.pricingPlan}>
+            <h2>Basic</h2>
+            <h3>$19.99/month</h3>
+            <ul>
+              <li>Access to gym during regular hours</li>
+              <li>Basic workout equipment</li>
+              <li>No personal training sessions</li>
+            </ul>
+            <button onClick={() => navigate('/register')}>Register</button>
+          </div>
+          <div className={style.pricingPlan}>
+            <h2>Premium</h2>
+            <h3>$49.99/month</h3>
+            <ul>
+              <li>24/7 gym access</li>
+              <li>Full range of workout equipment</li>
+              <li>1 personal training session/month</li>
+            </ul>
+            <button onClick={() => navigate('/register')}>Register</button>
+          </div>
+          <div className={style.pricingPlan}>
+            <h2>Elite</h2>
+            <h3>$99.99/month</h3>
+            <ul>
+              <li>24/7 gym access</li>
+              <li>Full range of workout equipment</li>
+              <li>2 personal training sessions/month</li>
+            </ul>
+            <button onClick={() => navigate('/register')}>Register</button>
+          </div>
+        </div>
+      </div>
     </div>
 
-    
-    
-    );
+
+
+  );
 }
 
 export default GymPricing;
