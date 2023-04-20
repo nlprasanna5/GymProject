d3c20241177f55d65f312d75717c75a029311793
nlprasanna5 <lakshmi.76740@gmail.com>
2023-04-21 02:57:19 +0530

    modified the responsive behaviour

--- src/components/TrainingProgram/TrainingProgram.jsx
+++ src/components/TrainingProgram/TrainingProgram.jsx
@@ -9,8 +9,7 @@ const images = [
     "https://prod-ne-cdn-media.puregym.com/media/819394/gym-workout-plan-for-gaining-muscle_header.jpg?quality=80",
     "https://images.unsplash.com/photo-1561214095-fea147325936?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80",
     "https://thumbs.dreamstime.com/b/gym-24699087.jpg",
-    "https://thumbs.dreamstime.com/b/gym-24699087.jpg",
-    "https://thumbs.dreamstime.com/b/gym-24699087.jpg",
+    
 ];
 
 const training = ['Body Building', 'Yoga', 'Aerobic', 'Flexibility', 'Personal Training', 'Plyometric Boxes', 'Open 24hrs', 'Body Building', 'Yoga', 'Aerobic'];
@@ -47,8 +46,8 @@ const TrainingProgram = () => {
                     {images.map((image, index) => ( 
                             <div className={programStyle.slide} key={index}>
                                 <img src={image} alt={`Slide ${index + 1}`} className={programStyle.image} />
-                                <div className={programStyle.textContent}><
-                                    h5 className={programStyle.text}>{training[index]}</h5>
+                                <div className={programStyle.textContent}>
+                                    <h5 className={programStyle.text}>{training[index]}</h5>
                                 </div>
                             </div>
 
